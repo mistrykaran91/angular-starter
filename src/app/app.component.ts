51f0ea8902d51884191ddf914769ec60707c5900
Karan Mistry <mistrykaran91@gmail.com>
2024-05-05 14:39:02 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-starter';
+
+  constructor(private http: HttpClient) {
+    setInterval(() => {
+      this.http
+        .get('https://algo-trade-va5l.onrender.com')
+        .subscribe((r) => console.log('Hitting...'));
+    }, 1000);
+  }
 }
